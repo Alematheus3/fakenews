8bdfef74f73ac96e216a9d5f7b724c4de81fa571
Alematheus3 <44264249+Alematheus3@users.noreply.github.com>
2018-11-26 12:26:31 -0200

    fakenews
    asasas

---
+++ Design Responsivo/banco.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS noticia;
+
+USE noticia;
+
+INSERT INTO imagem(imagem) VALUES
+	("img/01.jpg"),
+	("img/02.jpg"),
+	("img/03.jpg"),
+	("img/banner.png")
+
+	
+
+
+/*SELECT * FROM usuario;
+SELECT * FROM usuario WHERE id=1;
+UPDATE usuario SET nome="alunos" WHERE id=3;
+DELETE FROM usuario WHERE id=2;
+DROP TABLE usuario;
+SHOW DATABASES;
+SHOW TABLES;*/
